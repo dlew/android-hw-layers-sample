e2693a45d1ea4e681cf6b252ea9953542484647d
Dan Lew <danlew42@gmail.com>
2015-01-10 11:25:45 -0600

    Added complex animation toggle

--- app/src/main/java/net/danlew/hwlayers/MainActivity.java
+++ app/src/main/java/net/danlew/hwlayers/MainActivity.java
@@ -14,9 +14,21 @@ import android.widget.TextView;
 public class MainActivity extends ActionBarActivity {
 
     private View mLayout1;
-    private View mLayout2;
+    private View mLoremIpsum1;
 
+    private View mLayout2;
+    private View mLoremIpsum2;
+
+    // When checked, this enables using hardware layers on the main layouts
     private CheckBox mHwLayerCheckbox;
+
+    // When checked, the animation gets a bit more complex - not only do the containers animate
+    // but one of the views inside each do their own independent animation. This will cause the HW
+    // layers to need to be constantly redrawn, which is detrimental to performance.
+    //
+    // In this case, the animation is simple enough that it still may run *close* to smooth, but if
+    // there were a few extra Views or more work then this would chug.
+    private CheckBox mComplexAnimationCheckbox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +37,12 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         mHwLayerCheckbox = (CheckBox) findViewById(R.id.hw_layers_checkbox);
+        mComplexAnimationCheckbox = (CheckBox) findViewById(R.id.complex_anim_checkbox);
 
         mLayout1 = configureLayout(R.id.layout_1, R.drawable.bg_layout_1, R.string.layout1_title);
+        mLoremIpsum1 = mLayout1.findViewById(R.id.lorem_ipsum);
         mLayout2 = configureLayout(R.id.layout_2, R.drawable.bg_layout_2, R.string.layout2_title);
+        mLoremIpsum2 = mLayout2.findViewById(R.id.lorem_ipsum);
 
         // Start with one of the two hidden
         mLayout2.setAlpha(0);
@@ -59,10 +74,10 @@ public class MainActivity extends ActionBarActivity {
         final View layoutIn = layout1Alpha == 0 ? mLayout1 : mLayout2;
         final View layoutOut = layoutIn == mLayout1 ? mLayout2 : mLayout1;
 
-        int windowPixels = getResources().getDisplayMetrics().widthPixels;
+        int widthPixels = getResources().getDisplayMetrics().widthPixels;
         float scale = .5f;
 
-        layoutIn.setTranslationX(windowPixels / 2);
+        layoutIn.setTranslationX(widthPixels / 2);
         layoutIn.setScaleX(scale);
         layoutIn.setScaleY(scale);
 
@@ -90,7 +105,7 @@ public class MainActivity extends ActionBarActivity {
 
         layoutOut.animate()
             .alpha(0)
-            .translationX(-windowPixels / 2)
+            .translationX(-widthPixels / 2)
             .scaleX(scale)
             .scaleY(scale)
             .setListener(new AnimatorListenerAdapter() {
@@ -109,6 +124,33 @@ public class MainActivity extends ActionBarActivity {
                 }
             })
             .start();
+
+        if (mComplexAnimationCheckbox.isChecked()) {
+            final View ipsumIn = layoutIn == mLayout1 ? mLoremIpsum1 : mLoremIpsum2;
+            final View ipsumOut = layoutOut == mLayout2 ? mLoremIpsum2 : mLoremIpsum1;
+
+            int heightPixels = getResources().getDisplayMetrics().heightPixels;
+            int target = heightPixels / 2;
+
+            ipsumIn.setTranslationY(target);
+            ipsumIn.setAlpha(0);
+
+            ipsumIn.animate()
+                .translationY(0)
+                .alpha(1)
+                .start();
+
+            ipsumOut.animate()
+                .translationY(target)
+                .alpha(0)
+                .start();
+        }
+        else {
+            mLoremIpsum1.setTranslationY(0);
+            mLoremIpsum1.setAlpha(1);
+            mLoremIpsum2.setTranslationY(0);
+            mLoremIpsum2.setAlpha(1);
+        }
     }
 
 }
